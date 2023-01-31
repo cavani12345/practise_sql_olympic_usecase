aeccf9bc9aa160cf01aa6f4972bd151cb094c5c8
cavani12345 <66497457+cavani12345@users.noreply.github.com>
2023-01-31 17:39:55 +0200

    Update olympic.sql

--- olympic.sql
+++ olympic.sql
@@ -99,3 +99,32 @@ ON c.id = subquery.country_id
 GROUP BY subquery.season,c.country
 -- Order to show most events at the top
 ORDER BY events DESC;
+
+--  Write query to help understand how BMI differs by each summer sport
+-- add custom column named bmi_bucket, which splits up BMI into three groups: <.25, .25-.30, >.30
+
+-- Pull in sport, bmi_bucket, and athletes
+-- Uncomment the original query
+SELECT 
+	sport,
+    CASE WHEN weight/height^2*100 <.25 THEN '<.25'
+    WHEN weight/height^2*100 <=.30 THEN '.25-.30'
+    WHEN weight/height^2*100 >.30 THEN '>.30'
+    ELSE 'no weight recorded'
+    END AS bmi_bucket,
+    COUNT(DISTINCT athlete_id) AS athletes
+FROM summer_games AS s
+JOIN athletes AS a
+ON s.athlete_id = a.id
+GROUP BY sport, bmi_bucket
+ORDER BY sport, athletes DESC;
+
+-- Comment out the troubleshooting query
+/*
+SELECT 
+	height, 
+    weight, 
+    weight/height^2*100 AS bmi
+FROM athletes
+WHERE weight/height^2*100 IS NULL;
+*/
