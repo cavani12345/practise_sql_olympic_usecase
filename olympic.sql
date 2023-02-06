7d22996e33c3ecc01f777d385afda37fc6f07fb0
cavani12345 <66497457+cavani12345@users.noreply.github.com>
2023-02-06 14:57:36 +0200

    Update olympic.sql

--- olympic.sql
+++ olympic.sql
@@ -34,6 +34,34 @@ JOIN countries ct
 ON ct.id = sg.country_id
 GROUP BY ct.region;
 
+-- Pull country_gdp by region and country
+-- Calculate the global gdp
+-- Calculate percent of global gdp
+-- Calculate percent of gdp relative to its region
+-- Filter out null gdp values
+-- Show the highest country_gdp at the top
+
+-- Pull country_gdp by region and country
+SELECT 
+	region,
+    country,
+	SUM(gdp) AS country_gdp,
+    -- Calculate the global gdp
+    SUM(SUM(gdp)) OVER () AS global_gdp,
+    -- Calculate percent of global gdp
+    SUM(gdp) / SUM(SUM(gdp)) OVER () AS perc_global_gdp,
+    -- Calculate percent of gdp relative to its region
+    SUM(gdp)/SUM(SUM(gdp))OVER(PARTITION BY region) AS perc_region_gdp
+FROM country_stats AS cs
+JOIN countries AS c
+ON cs.country_id = c.id
+-- Filter out null gdp values
+WHERE gdp IS NOT NULL
+GROUP BY region, country
+-- Show the highest country_gdp at the top
+ORDER BY country_gdp DESC;
+
+
 -- -- Show max gdp per Across all years by  country
 SELECT 
 	country_id,
@@ -137,3 +165,27 @@ SELECT
 FROM athletes
 WHERE weight/height^2*100 IS NULL;
 */
+-- Bring in region, country, and gdp_per_million
+-- Output the worlds gdp_per_million
+-- Build the performance_index in the 3 lines below
+-- Filter for 2016 and remove null gdp values
+-- Bring in region, country, and gdp_per_million
+SELECT 
+    region,
+    country,
+    SUM(gdp) / SUM(pop_in_millions) AS gdp_per_million,
+    -- Output the worlds gdp_per_million
+    SUM(SUM(gdp)) OVER () / SUM(SUM(pop_in_millions)) OVER () AS gdp_per_million_total,
+    -- Build the performance_index in the 3 lines below
+    (SUM(gdp) / SUM(pop_in_millions))
+    /
+    (SUM(SUM(gdp)) OVER () / SUM(SUM(pop_in_millions)) OVER ()) AS performance_index
+-- Pull from country_stats_clean
+FROM country_stats_clean AS cs
+JOIN countries AS c 
+ON cs.country_id = c.id
+-- Filter for 2016 and remove null gdp values
+WHERE year = '2016-01-01' AND gdp IS NOT NULL
+GROUP BY region, country
+-- Show highest gdp_per_million at the top
+ORDER BY gdp_per_million DESC;
