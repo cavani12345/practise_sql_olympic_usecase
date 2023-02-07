75a24ee06e226718b7d68b6d40ef5343f9c270c6
cavani12345 <66497457+cavani12345@users.noreply.github.com>
2023-02-07 17:06:14 +0200

    Update olympic.sql

--- olympic.sql
+++ olympic.sql
@@ -189,3 +189,98 @@ WHERE year = '2016-01-01' AND gdp IS NOT NULL
 GROUP BY region, country
 -- Show highest gdp_per_million at the top
 ORDER BY gdp_per_million DESC;
+
+-- build a report that shows each country's month-over-month views
+-- include revenue per month
+-- Create previous_month_views that pulls the total views from last month for the given country
+--- percentage of current month relative to previous month revenue ( positive show grow, negative show a loss)
+SELECT
+	-- Pull month and country_id
+	date_part('month',date) AS month,
+	country_id,
+    -- Pull in current month views
+    SUM(views) AS month_views,
+    -- Pull in last month views
+    LAG(SUM(views)) OVER(PARTITION BY country_id ORDER BY date_part('month',date)) AS previous_month_views,
+    -- Calculate the percent change
+    SUM(views)/LAG(SUM(views)) OVER(PARTITION BY country_id ORDER BY date_part('month',date))-1 AS perc_change
+FROM web_data
+WHERE date <= '2018-05-31'
+GROUP BY month,country_id;
+ 
+ ---- Caclculate average view for the last 7 days for every date
+ SELECT
+	-- Pull in date and daily_views
+	date,
+	SUM(views) AS daily_views,
+    -- Calculate the rolling 7 day average
+	AVG(SUM(views)) OVER (ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS weekly_avg
+FROM web_data
+GROUP BY date;
+
+----- Output the value of weekly_avg from 7 days prior
+-- -- Calculate percent change vs previous period
+SELECT 
+	-- Pull in date and weekly_avg
+	date,
+    weekly_avg,
+    -- Output the value of weekly_avg from 7 days prior
+    LAG(weekly_avg,7) OVER (ORDER BY date) AS weekly_avg_previous,
+    -- Calculate percent change vs previous period
+    (weekly_avg/(LAG(weekly_avg,7) OVER (ORDER BY date)))-1 AS perc_change
+FROM
+  (SELECT
+      -- Pull in date and daily_views
+      date,
+      SUM(views) AS daily_views,
+      -- Calculate the rolling 7 day average
+      AVG(SUM(views)) OVER (ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS weekly_avg
+  FROM web_data
+  -- Alias as subquery
+  GROUP BY date) AS subquery
+-- Order by date in descending order
+ORDER BY date DESC;
+
+--- arrange athlete in same country with respect to their height tallest should named as 1
+SELECT 
+     	-- Pull in country_id and height
+        country_id, 
+        height, 
+        -- Number the height of each country's athletes
+        ROW_NUMBER() OVER (PARTITION BY country_id ORDER BY height DESC) AS row_num
+    FROM winter_games AS w 
+    JOIN athletes AS a ON w.athlete_id = a.id
+    GROUP BY country_id, height
+    -- Alias as subquery
+    ORDER BY country_id, height DESC
+    
+    --- calculate avg tallest height by region
+    -- Calculate region's percent of world gdp
+    
+    SELECT
+	-- Pull in region and calculate avg tallest height
+    region,
+    AVG(height) AS avg_tallest,
+    -- Calculate region's percent of world gdp
+    SUM(gdp)/SUM(SUM(gdp))OVER() AS perc_world_gdp   
+FROM countries AS c
+JOIN
+    (SELECT 
+     	-- Pull in country_id and height
+        country_id, 
+        height, 
+        -- Number the height of each country's athletes
+        ROW_NUMBER() OVER (PARTITION BY country_id ORDER BY height DESC) AS row_num
+    FROM winter_games AS w 
+    JOIN athletes AS a ON w.athlete_id = a.id
+    GROUP BY country_id, height
+    -- Alias as subquery
+    ORDER BY country_id, height DESC) AS subquery
+ON c.id = subquery.country_id
+-- Join to country_stats
+JOIN country_stats AS cs
+ON c.id = cs.country_id
+-- Only include the tallest height for each country
+WHERE row_num = 1
+GROUP BY region;
+    
